b84cf7cc6c5b512f36eb1b1505d4fb79a7b28bdd
Yvan Sanson <yvan.sanson@polytech.univ-montp2.fr>
2021-05-11 21:34:02 +0200

    Added AngleSharp for web scraping

---
+++ NyaapiDotnet/src/si/Scraper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+
+namespace NyaapiDotnet.src.si
+{
+    class Scraper
+    {
+    }
+}
